0809f9182a97d4b64cc9d97470bda7309b81e716
carlo moleka <me@carlomoleka.com>
2022-12-04 15:02:08 -0500

    fixed treesitter highlight issue

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.setup = function()
-  -- local configs = require('user.plugin-configs')
+  local configs = require('user.plugin-configs')
   lvim.plugins = {
     { "github/copilot.vim" },
     {
@@ -17,6 +17,36 @@ M.setup = function()
     },
     {
       "tpope/vim-commentary",
+    },
+    {
+      "sindrets/diffview.nvim",
+      cmd = { "DiffviewOpen", "DiffViewClose", "DiffviewToggleFiles", "DiffviewFocusFiles", "DiffviewRefresh" },
+      config = configs.diffview,
+    },
+    {
+      "nvim-treesitter/playground",
+      config = function()
+        require "nvim-treesitter.configs".setup {
+          playground = {
+            enable = true,
+            disable = {},
+            updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+            persist_queries = false, -- Whether the query persists across vim sessions
+            keybindings = {
+              toggle_query_editor = 'o',
+              toggle_hl_groups = 'i',
+              toggle_injected_languages = 't',
+              toggle_anonymous_nodes = 'a',
+              toggle_language_display = 'I',
+              focus_language = 'f',
+              unfocus_language = 'F',
+              update = 'R',
+              goto_node = '<cr>',
+              show_help = '?',
+            },
+          }
+        }
+      end,
     },
   }
 
